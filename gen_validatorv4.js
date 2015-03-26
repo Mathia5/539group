70d9f6adf44dd79da6e1d6815b71804f4516e6cb
longcarp <boxian@umich.edu>
2015-03-25 22:08:45 -0400

    new version detail validator

--- gen_validatorv4.js
+++ gen_validatorv4.js
@@ -1,21 +1,136 @@
-function yourDetailCheck(){
-	// document.getElementById("name").value="NULL";
-	// document.getElementById("CheckName").innerHTML="&nbsp&nbsp&nbsp&nbspNULL";
-	// check null for name
-	var nm=document.getElementById("name").value;
-	var nmRex=/\b[a-z\s]+/;
-	if(nm!="" & nmRex.test(nm)==true)
-	{
-			document.getElementById("CheckName").innerHTML="";
+function sum(obj){
+var count = 0;
+for(var i = 0; i < obj.length; i++)
+{
+    count = count + obj[i];
+}
+return count;
+}
 
-	}else if(nmRex.test(nm)!=true & nm!="")
+var JudgeDetail=[0,0,0,0,0,0];
+
+// detail validation check
+function yourDetailCheck(obj){
+
+// get value
+// var yourDetail=obj.children[1];
+var nm=obj.children[1].children[0].children[1].value;
+var ph=obj.children[1].children[1].children[1].value;
+var em1=obj.children[1].children[2].children[1].value;
+var em2=obj.children[1].children[3].children[1].value;
+var pw1=obj.children[1].children[4].children[1].value;
+var pw2=obj.children[1].children[5].children[1].value;
+
+
+// standard regex
+var nmRex=/\b[a-z\s{*}]+/;
+var phRex=/[0-9]{3}(\-|\s*)[0-9]{3}(\-|\s*)[0-9]{4}/;
+var emRex=/[\w-\._]+@[\w-_\.]+\.[\w-_]{2,4}/;
+
+
+
+// checking
+// name
+if(nm=="" | nm==null)
+{
+	obj.children[1].children[0].children[2].innerHTML="";
+	JudgeDetail[0]=0;
+}else if(nmRex.test(nm)!=true)
+{
+	obj.children[1].children[0].children[2].innerHTML="&nbsp&nbsp&nbsp&nbsp Invalid name";
+	JudgeDetail[0]=0;
+}else
+{
+    JudgeDetail[0]=1;
+    obj.children[1].children[0].children[2].innerHTML="";
+}
+
+//phone
+if(ph=="" | ph==null)
+{
+	obj.children[1].children[1].children[2].innerHTML="";
+	JudgeDetail[1]=0;
+
+}else if(phRex.test(ph)!=true)
+{
+	obj.children[1].children[1].children[2].innerHTML="&nbsp&nbsp&nbsp&nbsp Invalid number";
+	JudgeDetail[1]=0;
+}else
+{
+	JudgeDetail[1]=1;
+	obj.children[1].children[1].children[2].innerHTML="";
+}
+
+//email
+if(em1=="" | em1==null)
+{
+	obj.children[1].children[2].children[2].innerHTML="";
+	JudgeDetail[2]=0;
+
+}else if(emRex.test(em1)!=true)
+{
+	obj.children[1].children[2].children[2].innerHTML="&nbsp&nbsp&nbsp&nbsp Invalid email";
+	JudgeDetail[2]=0;
+}else
+{
+	JudgeDetail[2]=1;
+	obj.children[1].children[2].children[2].innerHTML="";
+}
+
+
+// email check
+if(em2=="" | em2==null)
+{
+	obj.children[1].children[3].children[2].innerHTML="";
+	JudgeDetail[3]=0;
+
+}else if(em1!==em2)
+{
+	obj.children[1].children[3].children[2].innerHTML="&nbsp&nbsp&nbsp&nbsp email doesn't match";
+	JudgeDetail[3]=0;
+}else
+{
+	JudgeDetail[3]=1;
+	obj.children[1].children[3].children[2].innerHTML="";
+}
+
+// password
+if(pw1=="" | pw1==null)
+{
+	obj.children[1].children[4].children[2].innerHTML="";
+	JudgeDetail[4]=0;
+
+}else
+{
+	JudgeDetail[4]=1;
+	obj.children[1].children[4].children[2].innerHTML="";
+}
+
+// password match
+if(pw2=="" | pw2==null)
+{
+	obj.children[1].children[5].children[2].innerHTML="";
+	JudgeDetail[5]=0;
+
+}else if(pw1!==pw2)
+{
+	obj.children[1].children[5].children[2].innerHTML="&nbsp&nbsp&nbsp&nbsp password doesn't match";
+	JudgeDetail[5]=0;
+}else
+{
+	JudgeDetail[5]=1;
+	obj.children[1].children[5].children[2].innerHTML="";
+}
+console.log(JudgeDetail);
+console.log(sum(JudgeDetail));
+}
+
+// overall check
+function overallCheck()
+{
+	if(sum(JudgeDetail)<6)
 	{
-		document.getElementById("CheckName").innerHTML="&nbsp&nbsp&nbsp&nbsp only contain char and space";
-	}else
-	{
-		document.getElementById("CheckName").innerHTML="&nbsp&nbsp&nbsp&nbsp Name is required";
+		alert("invalid input in your detail");
+		return false;
 	}
-
-	// var ph=document.getElementById("name").value;
-	// if()
 }
